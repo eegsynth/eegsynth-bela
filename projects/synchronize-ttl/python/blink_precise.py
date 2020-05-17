1e57d11d093f4ba2092297904eab5deb10070689
Robert Oostenveld <r.oostenveld@gmail.com>
2020-05-17 11:48:24 +0200

    added corresponding python code

---
+++ projects/synchronize-ttl/python/blink_precise.py
@@ -0,0 +1,18 @@
+from gpiozero import LED
+from time import sleep
+import pause
+from datetime import datetime,timedelta
+
+red = LED(17)
+
+now = datetime.now()
+next = datetime(now.year, now.month, now.day, now.hour, now.minute, now.second, 0)
+
+while True:
+    next = next + timedelta(seconds=1)
+    pause.until(next)
+    red.on()
+    sleep(0.2)
+    red.off()
+    sleep(0.2)
+    
