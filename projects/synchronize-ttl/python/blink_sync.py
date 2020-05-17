7e93d5e2fd2dde93235962e6651fe82b8d8fe674
Robert Oostenveld <r.oostenveld@gmail.com>
2020-05-17 14:33:47 +0200

    added a version that uses adjtime

--- projects/synchronize-ttl/python/blink_sync.py
+++ projects/synchronize-ttl/python/blink_sync.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
 
-from gpiozero import LED
+import os
+from gpiozero import LED, Button
 from time import sleep
 import pause
 from datetime import datetime,timedelta
 
 red = LED(17)
+button = Button(18)
+
+def upon_sync():
+    now = datetime.now()
+    print("%d.%06d" % (now.second, now.microsecond))
+    if now.microsecond < 500000:
+       os.system("sudo /home/pi/adjtime %d %d" % (0, -now.microsecond))
+    else:
+       os.system("sudo /home/pi/adjtime %d %d" % (0, +now.microsecond))
+
+button.when_pressed = upon_sync
 
 now = datetime.now()
 next = datetime(now.year, now.month, now.day, now.hour, now.minute, now.second, 0)
@@ -18,4 +30,4 @@ while True:
     red.off()
     sleep(0.2)
     next = next + timedelta(seconds=1)
-    
+
